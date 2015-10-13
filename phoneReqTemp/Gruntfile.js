c8df2504365f2cc5b6ca5aae4e2ac5379d024555
Rohan Chambers <rohanchambers888@gmail.com>
2015-10-13 17:44:07 +0100

    initial commit for playing around with Bootstrap 3 grid

---
+++ phoneReqTemp/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['*.js']
+		},		
+		uglify: {
+			dist: {
+				src: 'js/main.js',
+				dest: 'js/main.min.js'
+			}
+		},		
+		sass: {
+		    dist: {
+		      files: {
+		        'css/main.css': 'css/main.scss'
+		      }
+		    }
+		  },		  
+		cssmin: {
+		  combine: {
+		    files: {
+		      'css/styles.min.css': ['css/styles.css']
+		    }
+		  }
+		}				
+	});
+	
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');    
+  
+  // Default task(s).
+  grunt.registerTask('default', ['jshint','uglify', 'sass', 'cssmin']);	
+  
+};
