70382b87d199416da94129f5f100ce7bc7d005d9
Rohan Chambers <rohanchambers888@gmail.com>
2015-12-03 14:34:48 +0000

    angular changes

--- angular-seed/app/js/app.js
+++ angular-seed/app/js/app.js
@@ -14,3 +14,4 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
