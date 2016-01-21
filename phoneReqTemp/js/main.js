83c7185c4ab8cf79268e90986829ba097610a426
Rohan Chambers <rohanchambers888@gmail.com>
2016-01-21 16:12:50 +0000

    contact form comlete

--- phoneReqTemp/js/main.js
+++ phoneReqTemp/js/main.js
@@ -1,1 +1,21 @@
+$( document ).ready(function() {
 
+	// Prevent default
+	$('a, #btn-submit').click( function(e) {
+		e.preventDefault();
+	});
+
+	// Service shake repair products
+	$('.repair a').hover( function(){
+		$(this).parent().find('span').addClass('shake');
+	}, function() {
+		$(this).parent().find('span').removeClass();
+	});
+
+	// Shop shake products
+	$('.product a, .product img').hover( function(){
+		$(this).parent().find('img').addClass('shake');
+	}, function() {
+		$(this).parent().find('img').removeClass();
+	});
+});
