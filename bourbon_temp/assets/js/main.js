6e6ca95ef0bc4aa8fc9e7e7526a059755f97b95d
Rohan Chambers <rohanchambers888@gmail.com>
2016-06-07 09:23:39 +0100

    Sony form updates

--- bourbon_temp/assets/js/main.js
+++ bourbon_temp/assets/js/main.js
@@ -1,2 +1,69 @@
+// Age gate to allow users to access page depending on the age that they are.
+function ageGate() {
+    $('#dobYear').change(function(){
+        var minYear = 2002;
+        var selectedYear = $(this).find('option:selected').html();
+        console.log(selectedYear)
+        if(selectedYear > minYear) {
+            alert('I am sorry but you are not over the age of 14!');
+        }        
+    });
+}
+
+$.validator.addMethod("check_date_of_birth", function(value, element) {
+
+    var day = $("#dob_day").val();
+    var month = $("#dob_month").val();
+    var year = $("#dob_year").val();
+    var age =  14;
+
+    var mydate = new Date();
+    mydate.setFullYear(year, month-1, day-1);
+
+    var currdate = new Date();
+    currdate.setFullYear(currdate.getFullYear() - age);
+
+    return currdate > mydate;
+
+}, "You must be at least 14 years of age.");
+
 $(document).ready(function(){
+    // Constellations init
+    var canvasDiv = document.getElementById('particle-canvas');
+    
+    var options = {
+      particleColor: '#000',
+      interactive: true,
+      speed: 'high',
+      density: 'high'
+    };
+
+    var particleCanvas = new ParticleNetwork(canvasDiv, options);
+
+    // Show hide value of form inputs
+    $('#contact-form input').each(function(){
+        var txtval = $(this).val();
+        $(this).focus(function(){
+            if($(this).val() === txtval){
+                $(this).val('');
+                $(this).siblings('span').css({width: '100%'});
+            }
+        });
+        $(this).blur(function(){
+            if($(this).val() === ''){
+                $(this).val(txtval);
+                $(this).siblings('span').css({width: '0%'});
+            }
+        });
+    });
+
+    // jQuery validate form and submit form    
+    $('#contact-form').validate({
+        rules: {
+            dob_year: { check_date_of_birth: true }
+        },
+        submitHandler: function(form) {
+            form.submit(); //submit it the form
+        }
+    });
 });
---
+++ bourbon_temp/gruntfile.js
@@ -0,0 +1,135 @@
+module.exports = function(grunt){
+
+	// Load all of the tasks automatically using the matchdep dependency
+	//require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		// SASS task config
+		sass: {
+			dist: {
+				files: {
+					'assets/css/styles.css' : 'assets/sass/styles.scss'
+				}
+			}
+		},
+
+ 		// Watch task config
+		watch: {
+			css: {
+				files: 'assets/sass/**/*.scss',
+				tasks: ['sass', 'cssmin']
+			}
+
+            // javaScript: {
+            //     files: 'assets/js/**.js',
+            //     tasks: 'jshint'
+            // }
+		},
+
+		concat: {
+			css: {
+				src: ['bower_components/normalize-css/normalize.css', 'assets/css/styles.css'],
+				dest: 'assets/css/compiled.css'
+			},
+			js: {
+				options: {
+					separator: '\n;',
+				},
+				src: ['bower_components/jquery/dist/jquery.min.js', 'assets/js/vendor/**/*.js', 'assets/js/main.js'],
+				dest: 'assets/js/compiled.js'
+			}
+		},
+
+		// CSS min styles.css 
+		cssmin : {
+			minify : {
+				expand : true,
+				cwd : 'assets/css/',
+				src : [ '*.css', '!*.min.css' ],
+				dest : 'assets/css/',
+				ext : '.min.css'
+			}
+		},
+
+        // Minify JS
+        uglify: {
+            all: {
+                files: {
+                    'assets/js/compiled.min.js': 'assets/js/compiled.js',
+                }
+            }
+        },
+
+		// JShint for scripts
+        jshint: {
+            all: {
+                src: 'assets/js/main.js',
+	            options: {
+					camelcase: true,
+					noempty: true,
+					quotmark: 'single',
+				    curly: true,
+				    eqeqeq: true,
+				    eqnull: true,
+				    browser: true,
+				    globals: {
+				      jQuery: true
+				    }
+				}
+            }
+        },
+
+		// Using the BrowserSync Server for your static .html files.
+		browserSync: {
+		  default_options: {
+		    bsFiles: {
+		      src: [
+		        "assets/css/*.css",
+		        "assets/js/**/*.js",
+		        "*.php",
+		        "incl/*.php"
+		      ]
+		    },
+		    options: {
+		      watchTask: true,
+				proxy: "sdd.dev/index-dev.php"
+		    }
+		  }
+		},
+
+		// This is graet 
+		targethtml: {
+		  dist: {
+		    files: {
+		      'index.php': 'index-dev.php'
+		    }
+		  }
+		},
+
+		htmlmin: {                                     	 // Task
+			dist: {                                      // Target
+			  options: {                                 // Target options
+			  },
+			  files: {                           // Dictionary of files
+			    'index.php': 'index-dev.php'     // 'destination': 'source'
+			  }
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-browser-sync');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-targethtml');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+	grunt.registerTask('default', ['concat', 'cssmin', 'uglify', 'watch']);
+	//grunt.registerTask('default', ['browserSync', 'concat', 'cssmin', 'uglify', 'targethtml', 'watch']);
+
+};
