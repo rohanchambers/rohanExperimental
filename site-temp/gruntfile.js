d517e70b1f4d4660f102e6d2618fd48eca7f5deb
Rohan Chambers <rohanchambers888@gmail.com>
2017-03-14 12:50:34 +0000

    update grunt file for site temp grunt

--- site-temp/gruntfile.js
+++ site-temp/gruntfile.js
@@ -1,8 +1,5 @@
 module.exports = function(grunt){
 
-	// Load all of the tasks automatically using the matchdep dependency
-	//require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
