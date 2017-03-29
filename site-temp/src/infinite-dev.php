00becf45afd31ea4680c9564c6fc7199a5adc521
Rohan Chambers <rohanchambers888@gmail.com>
2017-03-29 17:51:19 +0100

    initial commit for infinite scrolling instagram account

---
+++ site-temp/src/infinite-dev.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Site template</title>
+    <meta name="title" content="">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <!--(if target dev)><!-->
+    <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <!--<!(endif)-->
+    <!--(if target dist)>
+    <link rel="stylesheet" href="../assets/css/compiled.min.css?{{rlsdate}}">
+    <!(endif)-->
+    <!--(if target dev)><!-->
+    <!-- <script src="../assets/js/vendor/modernizr-custom.js"></script> -->
+    <!--<!(endif)-->
+
+    <!-- Open graph tags -->
+    <meta property="og:image"           content="">
+    <meta property="og:title"           content="">
+    <meta property="og:description"     content="">
+    <meta property="og:url"             content="">
+    <meta property="og:image:width"     content="600">
+    <meta property="og:image:height"    content="600">
+    <meta property="og:image:type"      content="image/jpg">
+    <meta property="fb:app_id"          content="">
+
+    <meta name="twitter:card"           content="summary">
+    <meta name="twitter:site"           content="">
+    <meta name="twitter:title"          content="">
+    <meta name="twitter:description"    content="">
+    <meta name="twitter:creator"        content="">
+    <meta name="twitter:image:src"      content="">
+    <meta name="twitter:domain"         content="">
+</head>
+
+<body>
+    <section id="section-1">
+    	<div class="container">
+            <div class="row">
+    			<article id="element-1" class="col element">Third column</article>
+    			<article id="element-2" class="col element">Third column</article>
+    			<article id="element-3" class="col element">Third column</article>
+            </div>
+        </div><!-- End of container -->
+    </section>
+
+    <!--(if target dev)><!-->
+    <script src="../bower_components/jquery/dist/jquery.js"></script>
+    <script src="../bower_components/jquery-validation/dist/jquery.validate.js"></script>
+    <script src="../assets/js/main.js"></script>
+    <!--<!(endif)-->
+    <!--(if target dist)>
+    <script src="../assets/js/compiled.min.js?{{rlsdate}}"></script>
+    <!(endif)-->
+</body>
+</html>
