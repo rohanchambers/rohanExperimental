53c1c5f5d54cc3f8ed0cee1de01f0e58391ec659
Rohan Chambers <rohanchambers888@gmail.com>
2018-01-24 18:56:30 +0000

    initial commit

--- site-temp-barba/src/index-dev.php
+++ site-temp-barba/src/index-dev.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Site template Neat 2.1</title>
+    <title>Site template Barba JS</title>
     <meta name="title" content="">
     <meta name="author" content="">
     <meta name="description" content="">
