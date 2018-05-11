acc857805ad6480f6ecaacd321f62356cde3fa0a
Rohan Chambers <rohanchambers888@gmail.com>
2018-05-11 13:51:15 +0100

    scroll magic update to move on to new project

--- site-temp-neat/assets/js/main.js
+++ site-temp-neat/assets/js/main.js
@@ -3,12 +3,55 @@ var APP = {
 
 // // DOM ready
 $(function(){
-	APP.hamburger();
+	APP.scrollmagic();
 });
 
-APP.hamburger = function() {
-	$('.hamburger').click( function(){
-		$(this).toggleClass('is-active');
-		$('#myNav').toggleClass('isActive');
-	});
+APP.scrollmagic = function() {
+	// init controller
+	var controller = new ScrollMagic.Controller();
+
+	// create a scene
+	var pin = new ScrollMagic.Scene({
+	        duration: 2000,    // the scene should last for a scroll distance of 100px
+	        offset: 2000        // start this scene after scrolling for 50px
+	    })
+	    .setPin("#section-2") // pins the element for the the scene's 
+	    //.addIndicators()
+	    .addTo(controller); // assign the scene to the controller
+
+
+	var element = new ScrollMagic.Scene({
+			triggerElement: "#element-1", duration: 2000
+	    })
+		.setTween("#element-1", 0.5, {marginTop: "100px"}) // trigger a TweenMax.to tween
+	    .addTo(controller); // assign the scene to the controller
+
+
+	var dropdown = new ScrollMagic.Scene({
+			triggerElement: "#element-3", duration: 2000
+	    })
+		.setTween("#element-3", 1, {marginTop: "500px", opacity: 1}) // trigger a TweenMax.to tween
+	    .addTo(controller) // assign the scene to the controller
+	    .addIndicators()
+
+		var controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}			
+		});
+		// get all slides
+		var slides = document.querySelectorAll("section.section");
+		// var slidesID = document.getElementById("section#section-2");
+		// //console.log(slidesID)
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			var wipes = new ScrollMagic.Scene({
+
+					triggerElement: slides[i],
+				})
+				.setPin(slides[i])
+				.addIndicators() // add indicators (requires plugin)
+				.addTo(controller);
+		}
 };
--- site-temp-neat/src/index-dev.php
+++ site-temp-neat/src/index-dev.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Site template Neat 2.1</title>
+    <title>Site template Neat 2.1 | Scroll magic 2.0.5</title>
     <meta name="title" content="">
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <!--(if target dev)><!-->
     <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
@@ -21,88 +21,35 @@
     <!--(if target dev)><!-->
     <script src="../assets/js/vendor/modernizr-custom.js"></script>
     <!--<!(endif)-->
-
-    <!-- Open graph tags -->
-    <meta property="og:image"           content="">
-    <meta property="og:title"           content="">
-    <meta property="og:description"     content="">
-    <meta property="og:url"             content="">
-    <meta property="og:image:width"     content="600">
-    <meta property="og:image:height"    content="600">
-    <meta property="og:image:type"      content="image/jpg">
-    <meta property="fb:app_id"          content="">
-
-    <meta name="twitter:card"           content="summary">
-    <meta name="twitter:site"           content="">
-    <meta name="twitter:title"          content="">
-    <meta name="twitter:description"    content="">
-    <meta name="twitter:creator"        content="">
-    <meta name="twitter:image:src"      content="">
-    <meta name="twitter:domain"         content="">
 </head>
 
 <body>
-    <!-- The overlay -->
-    <div id="myNav" class="overlay">
-        <!-- Overlay content -->
-        <div class="overlay-content">
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-        </div>
-    </div>   
-
-    <div class="container">
-        <button class="hamburger hamburger--squeeze" type="button">
-            <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-            </span>
-        </button>
-    </div>
-
-    <section id="section-1" class="container">
+    <section id="section-1" class="section">
+        <h1>Section 1</h1>
 		<article id="element-1">Third column</article>
-		<article id="element-2">Third column</article>
-		<article id="element-3">Third column</article>
     </section>
 
-    <section id="section-2" class="container">
+    <section id="section-2" class="section">
+        <h1>Section 2</h1>
+        <article id="element-2">Third column</article>
+    </section>
+
+    <section id="section-3" class="section">
+        <h1>Section 3</h1>
+        <article id="element-3">Third column</article>
+    </section>
+
+    <section id="section-4" class="section">
+        <h1>Section 4</h1>
         <article id="element-4">Third column</article>
-        <article id="element-5">Third column</article>
-    </section>
-
-    <section id="section-3">
-        <div class="container">
-            <article id="element-6">Third column</article>
-        </div>
-    </section>
-
-    <section id="section-4">
-        <div class="full-container">
-            <article id="element-7">
-                <div class="container">
-                    <div>Third column</div>
-                </div>
-            </article>
-            <article id="element-8"></article>
-        </div>
-    </section>
-
-    <section id="section-5">
-        <div class="full-container">
-            <article id="element-8"></article>
-            <article id="element-7">
-                <div class="container">
-                    <div>Third column</div>
-                </div>
-            </article>
-        </div>
-    </section>
+    </section>    
 
     <!--(if target dev)><!-->
     <script src="../bower_components/jquery/dist/jquery.js"></script>
-    <script src="../bower_components/jquery-validation/dist/jquery.validate.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.14.2/TweenMax.min.js"></script>
+    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js"></script>
+    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
+    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js"></script>
     <script src="../assets/js/main.js"></script>
     <!--<!(endif)-->
     <!--(if target dist)>
