d77a8daa5c1ed4be14c64d78e48d25f2e64d4720
Rohan Chambers <rohanchambers888@gmail.com>
2018-08-05 18:31:04 +0100

    Pinning trials

--- site-temp-neat/assets/js/main.js
+++ site-temp-neat/assets/js/main.js
@@ -7,112 +7,132 @@ $(function(){
 });
 
 APP.scrollmagic = function() {
-	// init controller
-	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+	// Init controller
+	var controller = new ScrollMagic.Controller();
 
-	// // build scene
-	// var a = new ScrollMagic.Scene({
-	// 		triggerElement: "#parallax4",
-	// 		triggerHook: 0,
-	// 		duration: "250%"					
-	// 	})					
-	// 	.setPin("#parallax4")
-	// 	//.setTween(wipeAnimation)
-	// 	.addIndicators() // add indicators (requires plugin)
-	// 	.addTo(controller);
-
-
-	// build scenes
-	new ScrollMagic.Scene({triggerElement: "#parallax1"})
+	new ScrollMagic.Scene({
+				triggerElement: "#parallax1", 
+				triggerHook: "onEnter",
+				duration: "200%"})
 					.setTween("#parallax1 > .image", {y: "80%", ease: Linear.easeNone})
 					.addIndicators()
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#parallax2"})
+	new ScrollMagic.Scene({
+				triggerElement: "#parallax2", 
+				triggerHook: "onEnter",
+				duration: "200%"})
 					.setTween("#parallax2 > .image", {y: "0%", ease: Linear.easeNone})
 					.addIndicators()
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#parallax3"})
+	new ScrollMagic.Scene({
+				triggerElement: "#parallax3", 
+				triggerHook: "onEnter",
+				duration: "200%"})
 					.setTween("#parallax3 > .image", {y: "80%", ease: Linear.easeNone})
 					.addIndicators()
 					.addTo(controller);
 
-	new ScrollMagic.Scene({triggerElement: "#parallax4"})
-					.setTween("#parallax4 > .image", {y: "0%", ease: Linear.easeNone})
-					.addIndicators()
-					.addTo(controller);	
+	new ScrollMagic.Scene({
+			triggerElement: "#parallax4", 
+			triggerHook: 0,
+			duration: "500%"
+			})
+			.setTween("#parallax4 > .image", {y: "0%", ease: Linear.easeNone})
+			.setPin('#parallax4')
+			.addTo(controller);	
 
-	new ScrollMagic.Scene({triggerElement: "#parallax5"})
+			new ScrollMagic.Scene({
+					triggerElement: "#parallax4",
+					triggerHook: 0.25,
+					duration: "200%"
+				})
+				.setPin('#element-7')
+				.setTween("#element-7", 1, { top: "35%"}) // trigger a TweenMax.to tween
+				.addTo(controller);						
+
+			// new ScrollMagic.Scene({
+			// 		triggerElement: "#parallax4",
+			// 		offset: "200"
+			// 	})			
+			// 	//.setPin('#element-7')
+			// 	.addTo(controller);						
+
+
+
+
+
+
+
+	new ScrollMagic.Scene({
+				triggerElement: "#parallax5", 
+				triggerHook: "onEnter",
+				duration: "200%"})
 					.setTween("#parallax5 > .image", {y: "80%", ease: Linear.easeNone})
 					.addIndicators()
 					.addTo(controller);	
 
-	new ScrollMagic.Scene({triggerElement: "#parallax6"})
+	new ScrollMagic.Scene({
+				triggerElement: "#parallax6", 
+				triggerHook: "onEnter",
+				duration: "200%"})
 					.setTween("#parallax6 > .image", {y: "80%", ease: Linear.easeNone})
 					.addIndicators()
 					.addTo(controller);	
 
 
-
+// Elements
 	new ScrollMagic.Scene({
-			triggerElement: "#parallax2"
+			triggerElement: "#parallax2",
+			triggerHook: "onEnter",
+			duration: "200%"
 		})
-		.setTween("#element-1", 0.5, {backgroundColor: "green", bottom: "100%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "1 (duration: 5000)"}) // add indicators (requires plugin)
-		.addTo(controller);		
+			.setTween("#element-1", 0.25, {backgroundColor: "green", bottom: "100%"}) // trigger a TweenMax.to tween
+			.addIndicators()
+			.addTo(controller);		
 
-
-
-
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax2", duration: 500
-		})
-		.setTween("#element-2", 1, { bottom: "50%", left: "15%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
-		.addTo(controller);																				
-
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax2", duration: 500
-		})
-		.setTween("#element-3", 0.5, { bottom: "50%", left: "50%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "3 (duration: 0)"}) // add indicators (requires plugin)
-		.addTo(controller);				
-
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax2", duration: 500
-		})
-		.setTween("#element-4", 0.5, { bottom: "50%", left: "85%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "4 (duration: 0)"}) // add indicators (requires plugin)
-		.addTo(controller);		
 
 
 
 	new ScrollMagic.Scene({
-			triggerElement: "#parallax3"
-		})
-		.setTween("#element-5", 0.5, {backgroundColor: "green", bottom: "60%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "5 (duration: 5000)"}) // add indicators (requires plugin)
-		.addTo(controller);		
+		triggerElement: "#parallax2", 
+		triggerHook: 1,
+		duration: "200%"})
+			.setTween("#element-2", 0.5, { bottom: "35%", left: "15%", transform: "rotate(-10deg)"}) // trigger a TweenMax.to tween
+			.addTo(controller);																				
 
 	new ScrollMagic.Scene({
-			triggerElement: "#parallax3"
-		})
-		.setTween("#element-6", 0.5, {backgroundColor: "green"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "6 (duration: 5000)"}) // add indicators (requires plugin)
-		.addTo(controller);		
+		triggerElement: "#parallax2", 
+		triggerHook: 1,
+		duration: "200%"})
+			.setTween("#element-3", 0.5, { bottom: "40%", left: "50%"}) // trigger a TweenMax.to tween
+			.addTo(controller);				
 
-
+	new ScrollMagic.Scene({
+		triggerElement: "#parallax2", 
+		triggerHook: 1,
+		duration: "200%"})	
+			.setTween("#element-4", 0.5, { bottom: "35%", left: "60%", transform: "rotate(5deg)"}) // trigger a TweenMax.to tween
+			.addTo(controller);		
 
 
 
 	new ScrollMagic.Scene({
 			triggerElement: "#parallax3",
-			triggerHook: 0
+			duration: "200%"
 		})
-		.setTween("#element-7", 1, {backgroundColor: "orange", top: "30%"}) // trigger a TweenMax.to tween
-		.addIndicators({name: "7 (duration: 1000)"}) // add indicators (requires plugin)
-		.addTo(controller);				
+		.setTween("#element-5", 0.5, {backgroundColor: "green", bottom: "60%"}) // trigger a TweenMax.to tween
+		.addTo(controller);		
+
+	new ScrollMagic.Scene({
+			triggerElement: "#parallax3",
+			duration: "200%"
+		})
+		.setTween("#element-6", 0.5, {backgroundColor: "green"}) // trigger a TweenMax.to tween
+		.addTo(controller);		
+
+		
 
 }
 
