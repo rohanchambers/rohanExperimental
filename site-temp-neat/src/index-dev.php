d0feec527ceaf50319677eb391369d7069f92dd6
Rohan Chambers <rohanchambers888@gmail.com>
2018-10-25 14:28:08 +0100

    smooth state branch testing

--- site-temp-neat/src/index-dev.php
+++ site-temp-neat/src/index-dev.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Smooth State - Home</title>
+    <title>Smooth State - Home Testing Smooth</title>
     <meta name="title" content="">
     <meta name="author" content="">
     <meta name="description" content="">
