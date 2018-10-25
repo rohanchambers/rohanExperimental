f979a581da8dd4029a1b2ee223d05bb5f8c66d2f
Rohan Chambers <rohanchambers888@gmail.com>
2018-10-25 17:41:06 +0100

    updated with learning from gsap

--- site-temp-neat/assets/js/main.js
+++ site-temp-neat/assets/js/main.js
@@ -10,7 +10,7 @@ APP.gsap = function() {
     // Single tween
     var element1 = $('#element-1');
     var tween = TweenMax.to(element1, 1, {
-        x:500, rotation:360, 
+        x:500, rotation:360, repeat: 3,repeatDelay:2.5, 
         //scale: 0.5, opacity:0, 
         ease: Power2.easeInOut,
         //onComplete:animationComplete
@@ -20,9 +20,9 @@ APP.gsap = function() {
     var tl = new TimelineMax({repeat:3, delay:1});
 
     // Add 3 tweens that will play in direct succession.
-    tl.to("#element-2", 1, {x:750});
-    tl.to("#element-3", 1, {x:700});
-    tl.to("#element-4", 1, {x:650});
+    tl.to("#element-2", 1, {x:750, ease: Bounce.easeOut});
+    tl.to("#element-3", 1, {x:700, ease: Bounce.easeOut});
+    tl.to("#element-4", 1, {x:650, ease: Bounce.easeOut});
 
     // Call function on animation complete
     function animationComplete() {
