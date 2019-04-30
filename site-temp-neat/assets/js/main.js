07a55ce3f5d65bf6ec97f6f0c80525fcfbc93c97
Rohan Chambers <rohanchambers888@gmail.com>
2019-04-30 20:29:33 +0100

    wordpress remove this branch

--- site-temp-neat/assets/js/main.js
+++ site-temp-neat/assets/js/main.js
@@ -3,309 +3,52 @@ var APP = {
 
 // // DOM ready
 $(function(){
-	APP.scrollmagic();
-	APP.scrollToSections();
-	APP.smoothScrolling();
+	APP.hamburger();
+	APP.smoothState();
 });
 
+APP.smoothState = function() {
+  'use strict';
+  var $page = $('.page'),
 
-APP.smoothScrolling = function() {
-	var $window = $(window);		//Window object
-	
-	var scrollTime = 1.2;			//Scroll time
-	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
-		
-	$window.on("mousewheel DOMMouseScroll", function(event){
-		
-		event.preventDefault();	
-										
-		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
-		var scrollTop = $window.scrollTop();
-		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
-			
-		TweenMax.to($window, scrollTime, {
-			scrollTo : { y: finalScroll, autoKill:true },
-				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-				autoKill: true,
-				overwrite: 5							
-			});
-					
-	});
+      options = {
+        debug: true,
+        prefetch: true,
+        cacheLength: 2,
+        onStart: {
+          duration: 0, // Duration of our animation
+          render: function ($container) {
+            // Add your CSS animation reversing class
+            $container.addClass('is-exiting');
+            // Restart your animation
+            smoothState.restartCSSAnimations();
+          }
+        },
+        onProgress: {
+          // How long this animation takes
+          duration: 0,
+          // A function that dictates the animations that take place
+          render: function ($container) {
+            console.log('Testing this on progress!')
+          }
+        },        
+        onReady: {
+          duration: 0,
+          render: function ($container, $newContent) {
+            // Remove your CSS animation reversing class
+            $container.removeClass('is-exiting');
+            // Inject the new content
+            $container.html($newContent);
+          }
+        }
+      },
+      smoothState = $page.smoothState(options).data('smoothState');
+}
+
+APP.hamburger = function() {
+  $('.hamburger').click( function(){
+    console.log(1234)
+    $(this).toggleClass('is-active');
+    $('#myNav').toggleClass('isActive');
+  });
 };
-
-APP.scrollToSections = function() {
-	// Get scrolltop position
-	$(window).on("scroll", function() {
-	    console.log( $(this).scrollTop() );
-	});
-
-	// // Go to position
-	$("html, body").animate({
-		scrollTop: 1400
-	}, 500);
-
-	$("a[href^='#']").on("click", function(event) {
-	    var target = $( $(this).attr("href") );
-
-	    if( target.length ) {
-	        event.preventDefault();
-	        $("html, body").animate({
-	            scrollTop: target.offset().top
-	        }, 1500);
-	    }
-	});
-
-	$(".cta-element-8").click( function(){
-		$("html, body").animate({scrollTop:3660}, 1500);
-	});
-
-
-	$(".cta-element-9").click( function(){
-		$("html, body").animate({scrollTop:5770}, 1500);
-	});
-
-	$(".cta-element-10").click( function(){
-		$("html, body").animate({scrollTop:7570}, 1500);
-	});
-};
-
-APP.scrollmagic = function() {
-	// Init controller
-	var controller = new ScrollMagic.Controller();
-
-
-// ********** SLIDE 1 **********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax1",
-			triggerHook: "onEnter",
-			duration: "200%"})
-				.setTween("#parallax1 > .image", {y: "80%", ease: Linear.easeNone})
-				.addIndicators()
-				.addTo(controller);
-
-				new ScrollMagic.Scene({
-					triggerElement: "#parallax2",
-					triggerHook: "onEnter",
-					duration: "200%"
-				})
-					.setTween("#element-1", 0.25, {backgroundColor: "green", bottom: "100%"}) // trigger a TweenMax.to tween
-					.addIndicators()
-					.addTo(controller);
-
-// ********** SLIDE 2 **********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax2",
-			triggerHook: "onEnter",
-			duration: "200%"})
-				.setTween("#parallax2 > .image", {y: "0%", ease: Linear.easeNone})
-				.addIndicators()
-				.addTo(controller);
-
-				new ScrollMagic.Scene({
-					triggerElement: "#parallax2",
-					triggerHook: 0.75,
-					duration: "80%"})
-						.setTween("#element-2", 1, { bottom: "35%", left: "14%", transform: "rotate(-3deg)"})
-						.addTo(controller);
-
-				new ScrollMagic.Scene({
-					triggerElement: "#parallax2",
-					triggerHook: 0.75,
-					duration: "80%"})
-						.setTween("#element-3", 1, { bottom: "40%", left: "50%"})
-						.addTo(controller);
-
-				new ScrollMagic.Scene({
-					triggerElement: "#parallax2",
-					triggerHook: 0.75,
-					duration: "80%"})
-						.setTween("#element-4", 1, { bottom: "35%", right: "14%", transform: "rotate(3deg)"})
-						.addTo(controller);
-
-// ********** SLIDE 3 **********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax3",
-			triggerHook: "onEnter",
-			duration: "200%"})
-				.setTween("#parallax3 > .image", {y: "80%", ease: Linear.easeNone})
-				.addIndicators()
-				.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax3",
-						duration: "200%"
-					})
-					.setTween("#element-5", 0.5, {backgroundColor: "green", bottom: "80%"})
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax3",
-						duration: "200%"
-					})
-					.setTween("#element-6", 0.5, {backgroundColor: "green"})
-					.addTo(controller);
-
-
-// ********** SLIDE 4 BARLEY 360 ANIMATION**********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax4",
-			triggerHook: "onLeave",
-			duration: "800%"
-			})
-			.setTween("#parallax4 > .image", {y: "0%", ease: Linear.easeNone})
-			.setPin("#parallax4")
-			.addTo(controller);
-
-				// Barley 360 Pinned
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax4",
-						triggerHook: 0.25,
-						duration: "130%"
-					})
-					//.setPin('#element-7')
-					.setTween("#element-7", 1, { top: "35%"})
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax4",
-						triggerHook: 0.25,
-						duration: "250%",
-						offset: 1520
-					})
-					.setTween("#element-7", 1, { rotation: 360, ease: Linear.easeNone})
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax4",
-						triggerHook: "onCenter",
-						duration: "250%",
-						offset: 1000
-					})
-					.setTween("#element-8", 1, { bottom: "100%"})
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax4",
-						triggerHook: "onEnter",
-						duration: "250%",
-						offset: 3500
-					})
-					.setTween("#element-9", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax4",
-						triggerHook: "onEnter",
-						duration: "250%",
-						offset: 5300
-					})
-					.setTween("#element-10", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-
-// ********** SLIDE 5 **********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax5",
-			triggerHook: 0,
-			duration: "200%"
-			})
-			.setTween("#parallax5 > .image", {y: "0%", ease: Linear.easeNone})
-			.setPin("#parallax5")
-			.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "250%",
-						offset: 250
-					})
-					.setTween("#element-11", 1, { bottom: "110%"})
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "200%",
-						offset: -500
-					})
-					.setTween("#element-12", 10, { bottom: "35%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "150%",
-						offset: 250
-					})
-					.setTween("#element-13", 1, { bottom: "120%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "250%",
-						offset: 400
-					})
-					.setTween("#element-14", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "300%",
-						offset: 600
-					})
-					.setTween("#element-15", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "500%",
-						offset: 1000
-					})
-					.setTween("#element-16", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-				new ScrollMagic.Scene({
-						triggerElement: "#parallax5",
-						triggerHook: "onEnter",
-						duration: "250%",
-						offset: 1000
-					})
-					.setTween("#element-17", 1, { bottom: "100%"}) 
-					.addTo(controller);
-
-
-// ********** SLIDE 6 **********
-	new ScrollMagic.Scene({
-			triggerElement: "#parallax6",
-			triggerHook: "onEnter",
-			duration: "100%"})
-				.setTween("#parallax6 > .image", {y: "20%", ease: Linear.easeNone})
-				.addIndicators()
-				.addTo(controller);
-
-// ********** SLIDE 7 **********
-  new ScrollMagic.Scene({
-      triggerElement: "#parallax7",
-      triggerHook: "onEnter",
-      duration: "100%"})
-        .setTween("#parallax7 > .image", {y: "0%", ease: Linear.easeNone})
-        .addIndicators()
-        .addTo(controller);
-
-          new ScrollMagic.Scene({
-              triggerElement: "#parallax7",
-              triggerHook: "onEnter",
-              duration: "250%",
-              offset: 250
-            })
-            .setTween("#element-18", 1, { bottom: "110%"})
-            .addTo(controller);        
-};
-
-
-
-
-
--- site-temp-neat/src/index-dev.php
+++ site-temp-neat/src/index-dev.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Site template Neat 2.1 | Scroll magic 2.0.5 Testing</title>
+    <title>Smooth State - Home Testing Smooth</title>
     <meta name="title" content="">
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <!--(if target dev)><!-->
     <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
@@ -21,126 +21,101 @@
     <!--(if target dev)><!-->
     <script src="../assets/js/vendor/modernizr-custom.js"></script>
     <!--<!(endif)-->
+
+    <!-- Open graph tags -->
+    <meta property="og:image"           content="">
+    <meta property="og:title"           content="">
+    <meta property="og:description"     content="">
+    <meta property="og:url"             content="">
+    <meta property="og:image:width"     content="600">
+    <meta property="og:image:height"    content="600">
+    <meta property="og:image:type"      content="image/jpg">
+    <meta property="fb:app_id"          content="">
+
+    <meta name="twitter:card"           content="summary">
+    <meta name="twitter:site"           content="">
+    <meta name="twitter:title"          content="">
+    <meta name="twitter:description"    content="">
+    <meta name="twitter:creator"        content="">
+    <meta name="twitter:image:src"      content="">
+    <meta name="twitter:domain"         content="">
 </head>
 
 <body>
-    <!-- <div class="spacer s0"></div> -->
-    <div id="parallax1" class="parallaxParent">
-        <div class="image"></div>
-        <div id="element-1" class="element">
-            <h2>Lorem Ipsum (Slide 1)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus. Nullam mollis tempus purus, eu rhoncus lacus accumsan sit amet. </p>
-            <a href="#parallax2">Next slide</a>
-        </div>        
-    </div>
+    <div id="main" class="page">
+        <nav>
+            <a href="index-dev.php">Home</a>
+            <a href="about-dev.php">About</a>
+            <a href="services-dev.php">Services</a>
+            <a href="contact-dev.php">Contact</a>
+        </nav>     
+           
+        <div id="scene-a" class="animated fadeIn slow page-home">
+            <div>
+                <section id="section-1" class="container">
+                    <article id="element-1">Third column</article>
+                    <article id="element-2">Third column</article>
+                    <article id="element-3">Third column</article>
+                </section>
 
-    <div id="parallax2" class="parallaxParent">
-        <div class="content">
-            <h2>Lorem Ipsum  (Slide 2)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-            <a href="#parallax3">Next slide</a>
-        </div>   
-        <div class="image"></div>
+                <section id="section-2" class="container">
+                    <article id="element-4">Third column</article>
+                    <article id="element-5">Third column</article>
+                </section>
 
-        <div id="element-2" class="element">
-            <h2>Lorem Ipsum 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>   
+                <section id="section-3">
+                    <div class="container">
+                        <article id="element-6">Third column</article>
+                    </div>
+                </section>
 
-        <div id="element-3" class="element">
-            <h2>Lorem Ipsum 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>   
+                <section id="section-4">
+                    <div class="full-container">
+                        <article id="element-7">
+                            <div class="container">
+                                <div>Third column</div>
+                            </div>
+                        </article>
+                        <article id="element-8"></article>
+                    </div>
+                </section>
 
-        <div id="element-4" class="element">
-            <h2>Lorem Ipsum 3</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>                           
-    </div>
-
-    <div id="parallax3" class="parallaxParent">    
-        <div id="element-5" class="element">
-            <h2>Lorem Ipsum (Slide 3)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-            <span class="cta-element-8">Next slide</span>
-        </div>   
-
-        <div id="element-6" class="element"></div>           
-        <div class="image"></div>
-    </div>
-
-    <div id="parallax4" class="parallaxParent">
-        <div id="element-7" class="element">
-            <h2>(Slide 4)</h2>
+                <section id="section-5">
+                    <div class="full-container">
+                        <article id="element-8"></article>
+                        <article id="element-7">
+                            <div class="container">
+                                <div>Third column</div>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+            </div>
         </div>  
-
-        <div id="element-8" class="element">
-            <h2>Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus. Nullam mollis tempus purus, eu rhoncus lacus accumsan sit amet.</p>
-            <span class="cta-element-9">Next slide</span>
-        </div>  
-
-        <div id="element-9" class="element">
-            <h2>Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus. Nullam mollis tempus purus, eu rhoncus lacus accumsan sit amet.</p>
-            <span class="cta-element-10">Next slide</span>
-        </div>  
-
-        <div id="element-10" class="element">
-            <h2>Lorem Ipsum</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus. Nullam mollis tempus purus, eu rhoncus lacus accumsan sit amet.</p>
-            <a href="#parallax5">Next slide</a>
-        </div>                       
-
-        <div class="image"></div>
     </div>
 
-    <div id="parallax5" class="parallaxParent">
-        <div id="element-11" class="element"></div>  
-        <div id="element-12" class="element">
-            <h2>Lorem ipsum (Slide 5)</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus. Nullam mollis tempus purus, eu rhoncus lacus accumsan sit amet.</p>
-            <a href="#parallax6">Next slide</a>     
-        </div>  
-        <div id="element-13" class="element"></div>                
-        <div id="element-14" class="element"></div>                
-        <div id="element-15" class="element"></div> 
-        <div id="element-16" class="element"></div> 
-        <div id="element-17" class="element"></div>         
-        
-        <div class="image"></div>                                
+    <div class="container">
+        <button class="hamburger hamburger--squeeze" type="button">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>
     </div>
-
-    <div id="parallax6" class="parallaxParent">
-        <div class="content">
-            <h2>Lorem ipsum (Slide 6)</h2>
-            <p>Reveal Background Video Repeat</p>
-            <a href="#parallax1">Back to top</a>
-        </div>               
-        <div class="image"></div>
-    </div>
-
-    <div id="parallax7" class="parallaxParent">
-        <div class="content">
-            <h2>Lorem ipsum (Slide 7)</h2>
-            <p>Experimental section</p>
-            <a href="#parallax1">Back to top</a>
-        </div>               
-        <div class="image"></div>
-    </div>
-
-
-    <footer>Protoype parallax | Slam Dunk Digital 2018<!-- site by Slam Dunk Digital --></footer>
+    
+    <!-- The overlay -->
+    <div id="myNav" class="overlay">
+        <!-- Overlay content -->
+        <div class="overlay-content">
+            <a href="index-dev.php">Home</a>
+            <a href="about-dev.php">About</a>
+            <a href="services-dev.php">Services</a>
+            <a href="contact-dev.php">Contact</a>
+        </div>
+    </div>          
     <!--(if target dev)><!-->
     <script src="../bower_components/jquery/dist/jquery.js"></script>
-    <script src="../bower_components/gsap/src/minified/TweenMax.min.js"></script>
-    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/ScrollMagic.js"></script>
-    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js"></script>
-    <script src="../bower_components/gsap/src/minified/plugins/CSSPlugin.min.js"></script>
-    <script src="../bower_components/gsap/src/minified/plugins/ScrollToPlugin.min.js"></script>    
-    <script src="../bower_components/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js"></script>
+    <script src="../assets/js/vendor/jquery.smoothState.min.js"></script>
     <script src="../assets/js/main.js"></script>
-
     <!--<!(endif)-->
     <!--(if target dist)>
     <script src="../assets/js/compiled.min.js?{{rlsdate}}"></script>
