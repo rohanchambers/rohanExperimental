45ed2223b8dbb31fd6b34b1cd0981f381ebdde2b
Rohan Chambers <rohanchambers888@gmail.com>
2019-10-28 15:07:03 +0000

    commit to be able to switch branch

--- site-temp-neat/assets/js/main.js
+++ site-temp-neat/assets/js/main.js
@@ -1,50 +1,14 @@
 var APP = {
 };
 
-// DOM Ready
+// // DOM ready
 $(function(){
-    APP.gsap();
+	APP.hamburger();
 });
 
-APP.gsap = function() {
-
-    // Single tween
-    var element1 = $('#element-1');
-    
-    // Text plugin replaces yourElement's text with "This is the new text" over the course of 2 seconds
-    TweenLite.to("h1", 2, {text:"Rohan Chambers", delay: 2, ease:Bounce.easeOut});
-
-    var tween = TweenMax.to(element1, 1, {
-        x:500, rotation:360, repeat: 3,repeatDelay:2.5, 
-        //scale: 0.5, opacity:0, 
-        ease: Power2.easeInOut,
-        //onComplete:animationComplete
-    });
-
-    // Multiple tweens with timeline
-    var tl = new TimelineMax({repeat:3, delay:1});
-
-    // Add 3 tweens that will play in direct succession.
-    tl.to("#element-2", 1, {x:750, ease: Bounce.easeOut});
-    tl.to("#element-3", 1, {x:700, ease: Bounce.easeOut});
-    tl.to("#element-4", 1, {x:650, ease: Bounce.easeOut});
-
-    // Call function on animation complete
-    function animationComplete() {
-        alert('Animation complete')
-    }
-
-    // Controls
-    $('#btn-pause').click(function(){
-        tween.pause();
-    });
-
-    $('#btn-play').click(function(){
-        tween.play();
-    });
-
-    $('#btn-restart').click(function(){
-        tween.restart();
-    });
-    
+APP.hamburger = function() {
+	$('.hamburger').click( function(){
+		$(this).toggleClass('is-active');
+		$('#myNav').toggleClass('isActive');
+	});
 };
--- site-temp-neat/src/index-dev.php
+++ site-temp-neat/src/index-dev.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Site template Neat 2.1 | GSAP 2 Testing</title>
+    <title>Site template Neat 2.1</title>
     <meta name="title" content="">
     <meta name="author" content="">
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <!--(if target dev)><!-->
     <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
@@ -21,44 +21,89 @@
     <!--(if target dev)><!-->
     <script src="../assets/js/vendor/modernizr-custom.js"></script>
     <!--<!(endif)-->
+
+    <!-- Open graph tags -->
+    <meta property="og:image"           content="">
+    <meta property="og:title"           content="">
+    <meta property="og:description"     content="">
+    <meta property="og:url"             content="">
+    <meta property="og:image:width"     content="600">
+    <meta property="og:image:height"    content="600">
+    <meta property="og:image:type"      content="image/jpg">
+    <meta property="fb:app_id"          content="">
+
+    <meta name="twitter:card"           content="summary">
+    <meta name="twitter:site"           content="">
+    <meta name="twitter:title"          content="">
+    <meta name="twitter:description"    content="">
+    <meta name="twitter:creator"        content="">
+    <meta name="twitter:image:src"      content="">
+    <meta name="twitter:domain"         content="">
 </head>
 
 <body>
+    <!-- The overlay -->
+    <div id="myNav" class="overlay">
+        <!-- Overlay content -->
+        <div class="overlay-content">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+    </div>   
 
     <div class="container">
-        <h1>Hello GSAP this is Rohan Chambers playing with you!</h1>
-        <p>
-            <a href="#" id="btn-restart">Restart</a>
-            <a href="#" id="btn-pause">Pause</a>
-            <a href="#" id="btn-play">Play</a>
-        </p>
-        <div id="element-1" class="element">
-            <h2>Lorem Ipsum 1</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>
-
-        <div id="element-2" class="element">
-            <h2>Lorem Ipsum 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>
-
-        <div id="element-3" class="element">
-            <h2>Lorem Ipsum 3</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>
-
-        <div id="element-4" class="element">
-            <h2>Lorem Ipsum 3</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam luctus libero nec nibh rhoncus, nec hendrerit turpis cursus.</p>
-        </div>
+        <button class="hamburger hamburger--squeeze" type="button">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>
     </div>
 
-    <!--(if target dev)><!-->
-    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="../bower_components/gsap/src/minified/TweenMax.min.js"></script>
-    <script src="../bower_components/gsap/src/minified/plugins/TextPlugin.min.js"></script>
-    <script src="../assets/js/main.js"></script>
+    <section id="section-1" class="container">
+		<article id="element-1">Third column</article>
+		<article id="element-2">Third column</article>
+		<article id="element-3">Third column</article>
+    </section>
 
+    <section id="section-2" class="container">
+        <article id="element-4">Third column</article>
+        <article id="element-5">Third column</article>
+    </section>
+
+    <section id="section-3">
+        <div class="container">
+            <article id="element-6">Third column</article>
+        </div>
+    </section>
+
+    <section id="section-4">
+        <div class="full-container">
+            <article id="element-7">
+                <div class="container">
+                    <div>Third column</div>
+                </div>
+            </article>
+            <article id="element-8"></article>
+        </div>
+    </section>
+
+    <section id="section-5">
+        <div class="full-container">
+            <article id="element-8"></article>
+            <article id="element-7">
+                <div class="container">
+                    <div>Third column</div>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <!--(if target dev)><!-->
+    <script src="../bower_components/jquery/dist/jquery.js"></script>
+    <script src="../bower_components/jquery-validation/dist/jquery.validate.js"></script>
+    <script src="../assets/js/main.js"></script>
     <!--<!(endif)-->
     <!--(if target dist)>
     <script src="../assets/js/compiled.min.js?{{rlsdate}}"></script>
